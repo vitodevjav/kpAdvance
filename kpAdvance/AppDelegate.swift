3121e758d9d6dea4cb1a01569478558c3b7263bc
Vitaly Kazakevich <qulix.vitalik.kazakevich@gmail.com>
2019-01-21 17:19:57 +0300

    Remove storyboard

--- kpAdvance/AppDelegate.swift
+++ kpAdvance/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
         return true
     }
 }
