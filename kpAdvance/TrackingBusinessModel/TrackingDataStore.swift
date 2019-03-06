408f53eac763149784d21f77a00bdbf02177b677
Vitaly Kazakevich <qulix.vitalik.kazakevich@gmail.com>
2019-03-06 21:09:40 +0300

    MockTrackingDataStore

--- kpAdvance/TrackingBusinessModel/TrackingDataStore.swift
+++ kpAdvance/TrackingBusinessModel/TrackingDataStore.swift
@@ -11,5 +11,21 @@ import Foundation
 protocol TrackingDataStoreProtocol {
 	var trackingHistory: [String] { get }
 	func addTrackingInfo(_ trackingInfo: String)
-	func removeTrackingInfo(_trackingInfo: String)
+	func removeTrackingInfo(_ trackingInfo: String)
+}
+
+class MockTrackingDataStore: TrackingDataStoreProtocol {
+    private static var trackingHistory: [String] = []
+    var trackingHistory: [String] {
+        return MockTrackingDataStore.trackingHistory
+    }
+
+    func addTrackingInfo(_ trackingInfo: String) {
+        MockTrackingDataStore.trackingHistory.append(trackingInfo)
+    }
+
+    func removeTrackingInfo(_ trackingInfo: String) {
+        guard let indexToRemove = MockTrackingDataStore.trackingHistory.firstIndex(of: trackingInfo) else { return } // can't remove, no such item
+        MockTrackingDataStore.trackingHistory.remove(at: indexToRemove)
+    }
 }
