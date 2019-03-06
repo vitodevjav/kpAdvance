eca47f671c5908c62844d94300eccba2b7ffc382
Vitaly Kazakevich <qulix.vitalik.kazakevich@gmail.com>
2019-03-06 21:03:59 +0300

    Mock tracking timer

--- kpAdvance/TrackingBusinessModel/TrackingTimer.swift
+++ kpAdvance/TrackingBusinessModel/TrackingTimer.swift
@@ -9,7 +9,40 @@
 import Foundation
 
 protocol TrackingTimerProtocol {
+    var trackedTime: TimeInterval { get }
+
 	func start()
 	func pause()
 	func stop()
+}
+
+class MockTrackingTimer: TrackingTimerProtocol {
+    private var startDate: Date?
+    private var commitedTimeInterval: TimeInterval = 0.0
+    private var shouldResetBeforeTracking = false
+
+    var trackedTime: TimeInterval {
+        return commitedTimeInterval + Date().timeIntervalSince(startDate ?? Date())
+    }
+
+    func start() {
+        if startDate == nil {
+            commitedTimeInterval = 0.0
+        }
+        startDate = Date()
+    }
+
+    func pause() {
+        commitTrackingInterval()
+    }
+
+    func stop() {
+        commitTrackingInterval()
+        self.startDate = nil
+    }
+
+    private func commitTrackingInterval() {
+        guard let startDate = startDate else { return } // can't pause before stop
+        commitedTimeInterval += Date().timeIntervalSince(startDate)
+    }
 }
